6dcf40711b166c0eb4186ce2b40c5359166a9847
James Murphy <jim@jamespmurphy.com>
2025-06-26 15:39:46 -0400

    Use keywords

--- app/Csp/CustomPreset.php
+++ app/Csp/CustomPreset.php
@@ -15,9 +15,9 @@ class CustomPreset implements Preset
     {
         $policy->add(Directive::DEFAULT, Keyword::SELF)
             ->add(Directive::CONNECT, [Keyword::SELF, 'https://dpblu.com'])
-            ->add(Directive::SCRIPT, [Keyword::SELF, 'https://dpblu.com', 'unsafe-inline', 'unsafe-eval'])
-            ->add(Directive::IMG, [Keyword::SELF, 'https://dpblu.com', 'data: blob:'])
-            ->add(Directive::STYLE, [Keyword::SELF, 'unsafe-inline', 'https://fonts.bunny.net'])
+            ->add(Directive::SCRIPT, [Keyword::SELF, 'https://dpblu.com', Keyword::UNSAFE_INLINE, Keyword::UNSAFE_EVAL])
+            ->add(Directive::IMG, [Keyword::SELF, 'https://dpblu.com'])
+            ->add(Directive::STYLE, [Keyword::SELF, Keyword::UNSAFE_INLINE, 'https://fonts.bunny.net'])
             ->add(Directive::FONT, [Keyword::SELF, 'https://fonts.bunny.net'])
             ->add(Directive::FRAME_ANCESTORS, [Keyword::SELF])
             ->add(Directive::FRAME, [Keyword::SELF]);

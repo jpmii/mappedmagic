2b0bf632d15ddfbdbfd12c184c933e4689843123
James Murphy <jim@jamespmurphy.com>
2025-07-06 21:22:16 -0400

    Move API to Web route because it's session based

--- app/Http/Controllers/Api/WaitTimeController.php
+++ app/Http/Controllers/Api/WaitTimeController.php
@@ -5,11 +5,19 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Auth;
 
 class WaitTimeController extends Controller
 {
     public function index(Request $request)
     {
+        Log::info('WaitTimeController accessed', [
+            'user' => $request->user()?->id,
+            'park_ids' => $request->query('park_ids', []),
+            'authenticated' => Auth::check()
+        ]);
+        
         $parkIds = $request->query('park_ids', []);
         $waitTimes = [];
         
@@ -20,6 +28,8 @@ class WaitTimeController extends Controller
                 $waitTimes[$parkId] = $cached;
             }
         }
+        
+        Log::info('WaitTimeController returning data', ['wait_times_count' => count($waitTimes)]);
         
         return response()->json($waitTimes);
     }
--- routes/api.php
+++ routes/api.php
@@ -1,16 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\WaitTimeController;
 use Illuminate\Http\Request;
-
-Route::middleware([
-    'auth',
-    config('jetstream.auth_middleware'),
-    'verified'
-])->group(function () {
-    Route::get('/wait-times', [WaitTimeController::class, 'index']);
-});
 
 Route::middleware('auth')->get('/test-auth', function (Request $request) {
     return response()->json(['user' => $request->user()]);
